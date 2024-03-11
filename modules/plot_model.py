bdc0266622d02129ab32a06d48fc33cdcc77ee35
Irell Zane <itsirellzane@gmail.com>
2024-03-11 11:09:22 +0000

    Ridge with SGD_reg

---
+++ modules/plot_model.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.pipeline import make_pipeline
+
+xs = np.linspace(0, 3, 100).reshape(-1 , 1)
+
+def plot_model(X, y,model_class, polynomial, alphas, **model_kwargs):
+    for alpha, style, in zip(alphas, ("b:", "g--", "r-")):
+        if alpha > 0:
+            model = model_class(alpha, **model_kwargs)
+        else:
+            model = LinearRegression()
+        if polynomial:
+            model = make_pipeline(
+                PolynomialFeatures(degree=10, include_bias=False),
+                StandardScaler(),
+                model,
+            )
+        model.fit(X, y)
+        ys = model.predict(xs)
+        plt.plot(xs, ys, style, linewidth=2, label=f"alpha = {alpha}")
+        plt.axis([0, 3, 0, 3.5])
